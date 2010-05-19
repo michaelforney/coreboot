9207ca24b5836f135e8991a84084c6a25ddeeddf
Michael Forney <mforney@mforney.org>
2010-05-19 01:30:34 -0700

    Apply changes from r5448

--- src/mainboard/opus/m4/mptable.c
+++ src/mainboard/opus/m4/mptable.c
@@ -29,7 +29,7 @@ extern  unsigned sbdnb;
 static void *smp_write_config_table(void *v)
 {
 	static const char sig[4] = "PCMP";
-	static const char oem[8] = "OPUS    ";
+	static const char oem[8] = "COREBOOT";
 	static const char productid[12] = "M4          ";
 	struct mp_config_table *mc;
 	unsigned sbdn;
